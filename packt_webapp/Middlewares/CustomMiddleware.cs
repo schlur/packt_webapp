691b21bc6023be80e167f13e907384067babda4c
pat <pschlur@gmail.com>
2018-12-13 15:28:02 +0100

    first commit for packt_webmap

---
+++ packt_webapp/Middlewares/CustomMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace packt_webapp.Middlewares
+{
+    public class CustomMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly MyConfiguration _myconfig;
+
+        public CustomMiddleware(RequestDelegate next, IOptions<MyConfiguration> myconfig)
+        {
+            _next = next;
+            _myconfig = myconfig.Value;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            Debug.WriteLine($"---> Request asked for {httpContext.Request.Path} from {_myconfig.Firstname} {_myconfig.Lastname}");
+
+            // Call the next middleware delegate in the pipeline
+            await _next.Invoke(httpContext);
+        }
+    }
+}
---
+++ packt_webapp/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using packt_webapp.Middlewares;
+
+namespace packt_webapp
+{
+    public class MyConfiguration
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+    }
+    public class Startup
+    {
+
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment environment)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
+         //   Debug.WriteLine($" ---> From Config: {Configuration["firstname"]}");
+         //   Debug.WriteLine($" ---> From Config: {Configuration["withChild:option1"]}");
+        }
+
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+            services.Configure<MyConfiguration>(Configuration);
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseCustomMiddleware();
+
+            app.UseMvc();
+        }
+    }
+}
